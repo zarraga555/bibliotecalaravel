9663b0c88b79e0e146a9726f697b8821cccabaa3
zarraga555 <zarraga555@gmail.com>
2020-06-13 22:34:07 -0400

    bibliotecario Model

--- app/Http/Controllers/bibliotecarioController.php
+++ app/Http/Controllers/bibliotecarioController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Bibliotecario;
+use App\Http\Requests\BibliotecarioRequest;
 use Illuminate\Http\Request;
 
 class bibliotecarioController extends Controller
@@ -15,5 +16,48 @@ class bibliotecarioController extends Controller
     public function create (){
         return view('bibliotecario.create');
     }
+    public function store (BibliotecarioRequest $request){
+        Bibliotecario::create([
+            'ci' => request('ci'),
+            'complemento' => request('complemento'),
+            'nombre' => request('nombre'),
+            'direccion' => request('direccion'),
+            'telefono' => request('telefono'),
+            'correo' => request('correo'),
+            'turno' => request('turno'),
+            'salario' => request('salario'),
+            'fechaNacimiento' => request('fechaNacimiento'),
+            'paisNacimiento' => request('paisNacimiento'),
+            $request->validated()
+        ]);
+        return redirect()->route('bibliotecario.index');
+    }
 
+    public function edit (Bibliotecario $bibliotecarioitem){
+        return view('bibliotecario.edit', [
+            'bibliotecarioitem' => $bibliotecarioitem,
+        ] );
+    }
+
+    public function update(Bibliotecario $bibliotecarioitem, BibliotecarioRequest $request){
+        $bibliotecarioitem->update([
+            'ci' => request('ci'),
+            'complemento' => request('complemento'),
+            'nombre' => request('nombre'),
+            'direccion' => request('direccion'),
+            'telefono' => request('telefono'),
+            'correo' => request('correo'),
+            'turno' => request('turno'),
+            'salario' => request('salario'),
+            'fechaNacimiento' => request('fechaNacimiento'),
+            'paisNacimiento' => request('paisNacimiento'),
+            $request->validated()
+        ]);
+        return redirect()->route('bibliotecario.index');
+    }
+
+    public function destroy(Bibliotecario $bibliotecarioitem){
+        $bibliotecarioitem->delete();
+        return redirect()->route('bibliotecario.index');
+    }
 }
