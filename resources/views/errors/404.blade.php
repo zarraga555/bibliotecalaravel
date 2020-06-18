8211c915417d9f36ffc3378ba95ca2dc87e1393c
requeBernardo <requejpr.123@gmail.com>
2020-06-18 12:39:25 -0400

    creacion de error

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="content">
         <div class="row">
-            <h1>Pagina no encontrada</h1>
+            <h1>Pagina no encontrada.</h1>
         </div>
     </div>
 @endsection
