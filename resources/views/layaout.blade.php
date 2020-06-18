2eaf9517bbc7bedc89fe8d8ddfa879b5ce1cea6e
zarraga555 <zarraga555@gmail.com>
2020-06-17 22:12:20 -0400

    layaout modifications

--- resources/views/layaout.blade.php
+++ resources/views/layaout.blade.php
@@ -25,6 +25,9 @@
         .btn{
             padding: 2.5px 2.5px;
         }
+        table{
+            font-size: 0.82rem;
+        }
     </style>
 </head>
 <body>
@@ -35,5 +38,6 @@
         </div>
         @yield('content')
     </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 </body>
 </html>
