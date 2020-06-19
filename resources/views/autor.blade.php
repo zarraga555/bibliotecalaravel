0d08ac753afce3307cf5e729c3f63eeb694b0332
zarraga555 <zarraga555@gmail.com>
2020-06-19 13:42:05 -0400

    ajax was added in autor.blade(index)

--- resources/views/autor.blade.php
+++ resources/views/autor.blade.php
@@ -8,7 +8,8 @@
 <div class="col">
     {{-- <h1>{{__('Send Message') }}</h1> --}}
     <h1>@lang('Autores')</h1><br>
-    <a href=" {{ route('autor.create') }} " class="btn btn-primary">Nuevo Autor</a>
+    <a href="#"class="btn btn-primary" class="btn btn-primary" data-toggle="modal" data-target="#CreateModal">Nuevo Autor</a><br><br>
+    {{-- <a href=" {{ route('autor.create') }} " class="btn btn-primary">Nuevo Autor</a> --}}
     {{-- @if ($errors->any())
 @foreach($errors->all() as $error)
             {{ $error }}<br>
@@ -16,35 +17,117 @@
     @endif--}}
 </div>
 
-<div class="">
-    <table class="table table-responsive table">
-        <thead>
-            <tr>
-                <th scope="col">Nombre</th>
-                <th scope="col">Nacionalidad</th>
-                <th scope="col">Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                @forelse($autor as $autoritem)
-                    <td>{{ $autoritem->nombre }}</td>
-                    <td>{{ $autoritem->nacionalidad }}</td>
-                    <td>
-                        <a href="{{ route('autor.edit', $autoritem) }}"
-                            class="btn btn-success" title="Editar"><span class="material-icons">create</span></a>
-                        <a href="#"
-                            class="btn btn-danger" data-toggle="modal" data-target="#exampleModalCenter" title="Borrar"><span class="material-icons">delete</span></a>
-                    </td>
-            </tr>
-                @empty
-            <tr>
-                <td colspan="3" class="errortable" align="center">No hay datos disponibles</td>
-            </tr>
-                @endforelse
-        </tbody>
-    </table>
+<div id="message-update" class="alert alert-success alert-dismissible" role="alert" style="display:none">
+    <strong>Se ha actuzlizado correctamente</strong>
+</div>
 
+<div class="panel-body">
+    <div id="list-Autor"></div>
     {{-- {{ bibliotecario->links() }} --}}
 </div>
+@include('autor.modelEdit')
+@include('autor.modalCreate')
+
+
+
+
+<script>
+
+//-----------------------------------------------------
+//-------------MOSTRAR DATOS EN UNA TABLA--------------
+//-----------------------------------------------------
+
+    $(document).ready(function(){
+        listAutor();
+    });
+    var listAutor = function(){
+        $.ajax({
+            type:'get',
+            url:'{{ url('listAutor') }}',
+            success: function(data){
+                $('#list-Autor').empty().html(data);
+            }
+        });
+    }
+
+//-----------------------------------------------------
+//---------------------EDIT----------------------------
+//-----------------------------------------------------
+var Mostrar = function(autor){
+        var route = "{{ url('autor') }}/"+autor+"/edit";
+        $.get(route, function(data){
+            $("#idedit").val(data.id);
+            $("#nombreedit").val(data.nombre);
+            $("#nacionalidadedit").val(data.nacionalidad)
+        });
+    }
+
+//-----------------------------------------------------
+//----------------------UPDATE-------------------------
+//-----------------------------------------------------
+$("#actualizar").click(function(){
+    var id = $("#idedit").val();
+    var nombre = $("#nombreedit").val();
+    var nacionalidad = $("#nacionalidadedit").val();
+    var route = "{{ url('autor')}}/"+id+"";
+    var token = $("input[name=_token]").val();
+    $.ajax({
+        url: route,
+        headers: {'X-CSRF-TOKEN': token},
+        type: 'PUT',
+        dataType: 'json',
+        data: {
+            nombre: nombre,
+            nacionalidad: nacionalidad
+        },
+        success: function(data){
+            if(data.success == "true"){
+                listAutor();
+                // $('#CreateModal').modal('toggle');
+                $('#EditModal .close').click();
+                // alert("Agregado con exito");
+                $('#message-update').fadeIn();
+            }
+        },
+        error:function(data){
+            alert('campos vacios');
+        }
+    });
+});
+
+//-----------------------------------------------------
+//-----------------------STORE-------------------------
+//-----------------------------------------------------
+
+$('#enviar').click(function(){
+    var nombre = $("#nombre").val();
+    var nacionalidad = $("#nacionalidad").val();
+    var token = $("input[name=_token]").val();
+    var route = "{{ route('autor.store') }}"
+    $.ajax({
+        url: route,
+        headers:{'X-CSRF-TOKEN':token},
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            nombre:nombre,
+            nacionalidad:nacionalidad
+        },
+        success:function(data){
+            if(data.success == "true"){
+                listAutor();
+                $('#CreateModal .close').click();
+                $('#message-update').fadeIn();
+            }
+        }
+    });
+});
+
+//-----------------------------------------------------
+//---------------------ELIMINAR-------------------------
+//-----------------------------------------------------
+var Eliminar = function(autor){
+alert(autor);
+}
+</script>
 @endsection
