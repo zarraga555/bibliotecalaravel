59a679df99edf8dc0c8e5156d5b7a35af2a7c2ed
zarraga555 <zarraga555@gmail.com>
2020-06-19 13:26:19 -0400

    Controller changed in functions to work with ajax

--- app/Http/Controllers/autorController.php
+++ app/Http/Controllers/autorController.php
@@ -9,35 +9,67 @@ use Illuminate\Http\Request;
 class autorController extends Controller
 {
     public function index (){
+        // $autor = Autor::paginate(8);
+        return view('autor');
+    }
+
+    public function listAutor(){
         $autor = Autor::paginate(8);
-        return view('autor', compact('autor'));
+        return view('autor.listAutor', compact('autor'));
     }
 
     public function create (){
         return view('autor.create');
     }
+
     public function store (AutorRequest $request){
-        Autor::create([
-            'nombre' => request('nombre'),
-            'nacionalidad' => request('nacionalidad'),
-            $request->validated()
-        ]);
-        return redirect()->route('autor.index');
+        // Autor::create([
+        //     'nombre' => request('nombre'),
+        //     'nacionalidad' => request('nacionalidad'),
+        //     $request->validated()
+        // ]);
+        // return redirect()->route('autor.index');
+        if($request->ajax()){
+            //buscara el id que le mandamos
+            $result = Autor::create($request->all());
+
+            if($result){
+                return response()->json(['success' => 'true']);
+            }else{
+                return response()->json(['success' => 'false']);
+            }
+        }
     }
 
-    public function edit (Autor $autor){
-        return view('autor.edit', [
-            'autor' => $autor,
-        ] );
+    public function edit ($autor){
+        // return view('autor.edit', [
+        //     'autor' => $autor,
+        // ] );
+        $autoritem = Autor::find($autor);
+        return response()->json($autoritem);
     }
 
-    public function update(Autor $autor, AutorRequest $request){
-        $autor->update([
-            'nombre' => request('nombre'),
-            'nacionalidad' => request('nacionalidad'),
-            $request->validated()
-        ]);
-        return redirect()->route('autor.index');
+    public function update(AutorRequest $request, $id){
+        // $autor->update([
+        //     'nombre' => request('nombre'),
+        //     'nacionalidad' => request('nacionalidad'),
+        //     $request->validated()
+        // ]);
+        // return redirect()->route('autor.index');
+        if($request->ajax()){
+            //buscara el id que le mandamos
+            $autor = Autor::FindOrFail($id);
+            //guardo toda la informacion que me esta mandando
+            $input = $request->all();
+            //voy a guardar toda la informacion
+            $resuelt = $autor->fill($input)->save();
+
+            if($resuelt){
+                return response()->json(['success' => 'true']);
+            }else{
+                return response()->json(['success' => 'false']);
+            }
+        }
     }
 
     public function destroy(Autor $autor){
