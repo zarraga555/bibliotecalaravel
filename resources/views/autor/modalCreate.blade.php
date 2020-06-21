976aab69e0947f7a5bf6c84891806365e4200e40
zarraga555 <zarraga555@gmail.com>
2020-06-21 14:39:45 -0400

    Name correction

--- resources/views/autor/modalCreate.blade.php
+++ resources/views/autor/modalCreate.blade.php
@@ -4,7 +4,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Editar Categoria</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Crear Nuevo Autor</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -273,4 +273,4 @@
         </div>
     </div>
 </div>
-{{-- FINAL DEL MODAL DE ELIMINADOD --}}
+{{-- FINAL DEL MODAL DE AGREGADO --}}
