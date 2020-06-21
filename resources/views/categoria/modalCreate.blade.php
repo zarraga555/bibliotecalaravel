d1e1b4572da5e4c396cd780674e923c938649da1
zarraga555 <zarraga555@gmail.com>
2020-06-21 17:05:43 -0400

    Added modals for ajax use

---
+++ resources/views/categoria/modalCreate.blade.php
@@ -0,0 +1,31 @@
+<!-- MODAL DE AGREGADO -->
+<div class="modal fade" id="CreateModal" tabindex="-1" role="dialog" aria-labelledby="CreateModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Crear Categoria</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="FormCreate">
+                    {{-- method="POST" action="{{ route('autor.store') }}" --}}
+                    @csrf
+                    <div class="form-group">
+                        <label for="nombre">Nombre de la Categoria</label>
+                        <input type="text" name="nombre" id="nombre" class="form-control @error('nombre') is-invalid @enderror " value="{{ old('nombre') }}">
+                        {!! $errors->first('nombre', '<small>:message</small><br>') !!}
+                    </div>
+                    {{-- <button class="btn btn-primary">Enviar</button> --}}
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                <button type="button" id="enviar" class="btn btn-primary" >Guardar</button>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- FINAL DEL MODAL DE ELIMINADOD --}}
