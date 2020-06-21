e4d52866f80302f8e33f4d203ec64aed97ac2f37
zarraga555 <zarraga555@gmail.com>
2020-06-21 03:17:09 -0400

    CSS AND HTML MODIFICATIONS

--- resources/views/layaout.blade.php
+++ resources/views/layaout.blade.php
@@ -7,6 +7,8 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
@@ -32,18 +34,6 @@
             padding: 2.5px 2.5px;
         }
 
-        /* table{
-            font-size: 0.82rem;
-            margin: auto;
-        } */
-        table {
-            position: relative;
-            margin: auto;
-            width: 100%;
-            /*left:-15%*/
-            ;
-        }
-
         .wrapper {
             display: flex;
             position: relative;
@@ -59,7 +49,7 @@
             height: 100vh;
         }
 
-        @media (min-width: 768px) {
+        @media (min-width: 769px) {
             .navbar{
             height: 80px;
             }
@@ -71,6 +61,22 @@
 
         .btn-block+.btn-block {
             margin-top: 0rem;
+        }
+        #list-Autor{
+            position: relative;
+            margin: auto;
+        }
+        table{
+            position: relative;
+            margin: auto;
+            width: 100%;
+            display: inline-table;
+        }
+        .table-responsive {
+            width: 100%;
+            overflow-x: auto;
+            -webkit-overflow-scrolling: touch;
+            display: inline-table;
         }
         }
     </style>
