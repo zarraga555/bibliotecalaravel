ac9acb2025da9d37472bfe5f7135a923b6fa1e47
zarraga555 <zarraga555@gmail.com>
2020-06-21 17:06:55 -0400

    Modifications to the ajax call

--- resources/views/categoria.blade.php
+++ resources/views/categoria.blade.php
@@ -2,49 +2,168 @@
 
 @section('title')
     Categorias
-
 @endsection
 @section('formulario')
-<div class="col">
-    {{-- <h1>{{__('Send Message') }}</h1> --}}
-    <h1>@lang('Categorias')</h1><br>
-    <a href=" {{ route('categoria.create') }} " class="btn btn-primary">Nueva Categoria</a>
-    {{-- @if ($errors->any())
-@foreach($errors->all() as $error)
-            {{ $error }}<br>
-    @endforeach
-    @endif--}}
+<div class="container">
+    <div class="d-flex justify-content-between align-items-center mb-3">
+        <h1 style="align-items: center">Categorias</h1>
+        @auth
+            <h2><u><a href="#" class="btn btn-primary" class="btn btn-primary" data-toggle="modal" data-target="#CreateModal">Crear Nueva Categoria</a></u></h2>
+        @endauth
+    </div>
+
+    <div id="message-store" class="alert alert-success alert-dismissible" role="alert" style="display:none">
+        <strong>Se ha agregado correctamente</strong>
+    </div>
+    <div id="message-update" class="alert alert-success alert-dismissible" role="alert" style="display:none">
+        <strong>Se ha actuzlizado correctamente</strong>
+    </div>
+    <div id="message-destroy" class="alert alert-danger alert-dismissible" role="alert" style="display:none">
+        <strong>Se ha borrado el actor</strong>
+    </div>
+
+    <div class="panel-body">
+        <div id="list-Categoria" class="divTable"></div>
+    </div>
 </div>
 
-<div class="">
-    <table class="table table-responsive table">
-        <thead>
-            <tr>
-                <th scope="col">Codigo</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                @forelse($categoria as $categoriaitem)
-                    <td>{{ $categoriaitem->id }}</td>
-                    <td>{{ $categoriaitem->nombre }}</td>
-                    <td>
-                        <a href="{{ route('categoria.edit', $categoriaitem) }}"
-                            class="btn btn-success" title="Editar"><span class="material-icons">create</span></a>
-                        <a href="#"
-                            class="btn btn-danger" data-toggle="modal" data-target="#exampleModalCenter" title="Borrar"><span class="material-icons">delete</span></a>
-                    </td>
-            </tr>
-                @empty
-            <tr>
-                <td colspan="3" class="errortable" align="center">No hay datos disponibles</td>
-            </tr>
-                @endforelse
-        </tbody>
-    </table>
+@include('categoria.modalCreate')
+@include('categoria.modalEdit')
+@include('categoria.modalDelete')
 
-    {{-- {{ bibliotecario->links() }} --}}
-</div>
+<script>
+    //-----------------------------------------------------
+    //-----------------LIMPIADO DE LOS INPUTS--------------
+    //-----------------------------------------------------
+
+    function limpiarFormulario() {
+        document.getElementById("FormCreate").reset();
+    }
+
+    //-----------------------------------------------------
+    //-------------MOSTRAR DATOS EN UNA TABLA--------------
+    //-----------------------------------------------------
+
+    $(document).ready(function () {
+        listCategoria();
+    });
+    var listCategoria = function () {
+        $.ajax({
+            type: 'get',
+            url: '{{ url('listCategoria') }}',
+            success: function (data) {
+                $('#list-Categoria').empty().html(data);
+            }
+        });
+    }
+
+    //-----------------------------------------------------
+    //---------------------EDIT----------------------------
+    //-----------------------------------------------------
+    var Mostrar = function (categoria) {
+        var route = "{{ url('categoria') }}/" + categoria + "/edit";
+        $.get(route, function (data) {
+            $("#idedit").val(data.id);
+            $("#nombreedit").val(data.nombre);
+        });
+    }
+
+    //-----------------------------------------------------
+    //----------------------UPDATE-------------------------
+    //-----------------------------------------------------
+    $("#actualizar").click(function () {
+        var id = $("#idedit").val();
+        var nombre = $("#nombreedit").val();
+        var route = "{{ url('categoria') }}/" + id + "";
+        var token = $("input[name=_token]").val();
+        $.ajax({
+            url: route,
+            headers: {
+                'X-CSRF-TOKEN': token
+            },
+            type: 'PUT',
+            dataType: 'json',
+            data: {
+                nombre: nombre,
+            },
+            success: function (data) {
+                if (data.success == "true") {
+                    listCategoria();
+                    // $('#CreateModal').modal('toggle');
+                    $('#EditModal .close').click();
+                    // alert("Agregado con exito");
+                    $('#message-update').fadeIn();
+                    $('#message-update').show().delay(3000).fadeOut(1);
+                }
+            },
+            error: function (data) {
+                alert('campos vacios');
+            }
+        });
+    });
+
+    //-----------------------------------------------------
+    //-----------------------STORE-------------------------
+    //-----------------------------------------------------
+
+    $('#enviar').click(function () {
+        var nombre = $("#nombre").val();
+        var token = $("input[name=_token]").val();
+        var route = "{{ route('categoria.store') }}"
+        $.ajax({
+            url: route,
+            headers: {
+                'X-CSRF-TOKEN': token
+            },
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                nombre: nombre,
+            },
+            success: function (data) {
+                if (data.success == "true") {
+                    listCategoria();
+                    limpiarFormulario();
+                    $('#CreateModal .close').click();
+                    $('#message-store').fadeIn();
+                    $('#message-store').show().delay(3000).fadeOut(1);
+                }
+            }
+        });
+    });
+
+    //-----------------------------------------------------
+    //---------------------ELIMINAR------------------------
+    //-----------------------------------------------------
+    var Eliminar = function (categoria) {
+        var route = "{{ url('categoria') }}/" + categoria + "/edit";
+        $.get(route, function (data) {
+            $("#idDelete").val(data.id);
+        });
+    }
+
+    //-----------------------------------------------------
+    //---------------------DESTROY-------------------------
+    //-----------------------------------------------------
+    $('#delete').click(function () {
+        var categoria = $("#idDelete").val();
+        var token = $("input[name=_token]").val();
+        var route = "{{ URL('categoria') }}/" + categoria;
+        $.ajax({
+            url: route,
+            headers: {
+                'X-CSRF-TOKEN': token
+            },
+            type: 'DELETE',
+            success: function (data) {
+                if (data.success == "true") {
+                    listCategoria();
+                    $('#exampleModalCenter .close').click();
+                    $('#message-destroy').fadeIn();
+                    $('#message-destroy').show().delay(3000).fadeOut(1);
+                }
+            }
+        });
+    });
+</script>
 @endsection
