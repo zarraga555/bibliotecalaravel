79f868d885216ef76c4217e26d7dd8a96b403575
zarraga555 <zarraga555@gmail.com>
2020-06-22 02:28:17 -0400

    Modals replacing Create, Edit, Show and Delete

---
+++ resources/views/bibliotecario/listBibliotecario.blade.php
@@ -0,0 +1,35 @@
+<table class="table table-responsive table">
+    <thead>
+        <tr>
+            <th scope="col">CI</th>
+            <th scope="col">Compl.</th>
+            <th scope="col">Nombre Completo</th>
+            <th scope="col">Correo</th>
+            <th scope="col">Turno</th>
+            <th scope="col">Salario Bs</th>
+            <th scope="col">Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            @forelse($bibliotecario as $bibliotecarioitem)
+                <td>{{ $bibliotecarioitem->ci }}</td>
+                <td>{{ $bibliotecarioitem->complemento }}</td>
+                <td>{{ $bibliotecarioitem->nombre }}</td>
+                <td>{{ $bibliotecarioitem->correo }}</td>
+                <td>{{ $bibliotecarioitem->turno }}</td>
+                <td>{{ $bibliotecarioitem->salario }}</td>
+                <td>
+                    <a href="#" onclick="Ver({{$bibliotecarioitem->id}})" class="btn btnT btn-info"" title="Ver" data-toggle="modal" data-target="#ShowModal"><span class="material-icons">visibility</span></a>
+                    <a href="#" onclick='Mostrar({{$bibliotecarioitem->id}}) ' class="btn btnT btn-success" title="Editar" class="btn btn-primary" data-toggle="modal" data-target="#EditModal"><span class="material-icons">create</span></a>
+                    <a href="#" onclick='Eliminar({{$bibliotecarioitem->id}}) ' class="btn btnT btn-danger" data-toggle="modal" data-target="#exampleModalCenter" title="Borrar"><span class="material-icons">delete</span></a>
+                </td>
+        </tr>
+    @empty
+        <tr>
+            <td colspan="9" class="errortable" align="center">No hay datos disponibles</td>
+        </tr>
+        @endforelse
+    </tbody>
+</table>
+{{ $bibliotecario->links() }}
