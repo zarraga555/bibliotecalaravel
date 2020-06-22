207a445e0e948d4ed8390857b86b9e8de1e8bb81
zarraga555 <zarraga555@gmail.com>
2020-06-21 23:36:05 -0400

    Modals created for the use of Ajax

---
+++ resources/views/persona/listPersona.blade.php
@@ -0,0 +1,36 @@
+<table class="table table-responsive table">
+    <thead>
+        <tr>
+            <th scope="col">CI</th>
+            <th scope="col">Compl.</th>
+            <th scope="col">Nombre Completo</th>
+            <th scope="col">Direecion</th>
+            <th scope="col">Telefono</th>
+            <th scope="col">Correo</th>
+            <th scope="col">Fecha Nacimiento</th>
+            <th scope="col">Nacionalidad</th>
+            <th scope="col">Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            @forelse($persona as $personaitem)
+                <td>{{ $personaitem->ci }}</td>
+                <td>{{ $personaitem->complemento }}</td>
+                <td>{{ $personaitem->nombre }}</td>
+                <td>{{ $personaitem->direccion }}</td>
+                <td>{{ $personaitem->telefono }}</td>
+                <td>{{ $personaitem->correo }}</td>
+                <td>{{ $personaitem->fechaNacimiento }}</td>
+                <td>{{ $personaitem->paisNacimiento }}</td>
+                <td>
+                    <a href="#" onclick='Mostrar({{$personaitem->id}}) ' class="btn btn-success" title="Editar" class="btn btn-primary" data-toggle="modal" data-target="#EditModal"><span class="material-icons">create</span></a>
+                    <a href="#" onclick='Eliminar({{$personaitem->id}}) ' class="btn btn-danger" data-toggle="modal" data-target="#exampleModalCenter" title="Borrar"><span class="material-icons">delete</span></a>
+                </td>
+        </tr>
+            @empty
+                <td colspan="9" class="errortable" align="center">No hay datos disponibles</td>
+            @endforelse
+    </tbody>
+</table>
+{{ $persona->links() }}
