f226be9842eaa3660e856fae49d154b426483914
zarraga555 <zarraga555@gmail.com>
2020-06-29 12:57:55 -0400

    Se agrego el middleware en el kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \App\Http\Middleware\CheckRole::class,
+        'roles' => \App\Http\Middleware\CheckRoles::class,
     ];
 }
