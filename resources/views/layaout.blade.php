7bbfa0161e02165251bf07706b343cbae034bd39
zarraga555 <zarraga555@gmail.com>
2020-06-29 15:48:54 -0400

    etiqueta de css añadida

--- resources/views/layaout.blade.php
+++ resources/views/layaout.blade.php
@@ -7,6 +7,9 @@
     <title>@yield('title', 'Biblioteca')</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
+    {{-- <script src="https://kit.fontawesome.com/b99e675b6e.js"></script> --}}
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.2.0/css/all.css">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
@@ -15,6 +18,52 @@
     </script>
     <script src="/js/jquery.js "></script>
     <style>
+        .circle{
+            top: 85%;
+            left: 50%;
+            position: absolute;
+            transform: translate(-50%,-50%);
+            width: 100px;
+            height: 100px;
+            text-align: center;
+            line-height: 110px;
+            font-size: 50px;
+            overflow: hidden;
+        }
+        .circle .far{
+            color: #007bff!important;
+            margin: 0;
+            padding: 0;
+            animation: animate 1.5s linear infinite;
+        }
+        @keyframes animate{
+            0%{
+                transform: translateY(-50px);
+                text-shadow: 0 50px 0 #007bff;
+            }
+            20%{
+                transform: translateY(0px);
+                text-shadow: 0 10px 0 #007bff;
+            }
+            40%{
+                transform: translateY(0px);
+                text-shadow: 0 -10px 0 #007bff;
+            }
+            60%{
+                transform: translateY(0px);
+                text-shadow: 0 0px 0 #007bff;
+            }
+            100%{
+                transform: translateY(100px);
+                text-shadow: 0 50px 0 #007bff;
+            }
+        }
+        .row2{
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin-top: -85px;
+        }
         a {
             text-decoration: none;
         }
@@ -94,8 +143,9 @@
         </header>
         <main>
             @yield('formulario')
-            @yield('content')
+
         </main>
+        @yield('content')
         <footer class="bg-white text-center text-black-50 py-3 shadow">
             {{ config('app.name') }} | Copyright @ {{ date('Y') }}
         </footer>
